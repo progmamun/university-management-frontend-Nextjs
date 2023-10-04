21b37ac4f51f2f04eb1bbfed5de72603d5df09e0
progmamun <mamun333089@gmail.com>
2023-10-04 16:08:46 +0600

    part-4[done]

--- src/redux/api/departmentApi.ts
+++ src/redux/api/departmentApi.ts
@@ -1,3 +1,4 @@
+import { IDepartment, IMeta } from "@/types";
 import { tagTypes } from "../tag-types";
 import { baseApi } from "./baseApi";
 
@@ -5,6 +6,21 @@ const DEPARTMENT_URL = "/management-departments";
 
 export const departmentApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
+    departments: build.query({
+      query: (arg: Record<string, any>) => ({
+        url: DEPARTMENT_URL,
+        method: "GET",
+        params: arg,
+      }),
+      transformResponse: (response: IDepartment, meta: IMeta) => {
+        return {
+          departments: response,
+          meta,
+        };
+      },
+      providesTags: [tagTypes.department],
+    }),
+
     addDepartment: build.mutation({
       query: (data) => ({
         url: DEPARTMENT_URL,
@@ -13,7 +29,41 @@ export const departmentApi = baseApi.injectEndpoints({
       }),
       invalidatesTags: [tagTypes.department],
     }),
+
+    // get single department by id
+    department: build.query({
+      query: (id) => ({
+        url: `${DEPARTMENT_URL}/${id}`,
+        method: "GET",
+      }),
+      providesTags: [tagTypes.department],
+    }),
+
+    // update single department by id
+    updateDepartment: build.mutation({
+      query: (data) => ({
+        url: `${DEPARTMENT_URL}/${data.id}`,
+        method: "PATCH",
+        data: data.body,
+      }),
+      invalidatesTags: [tagTypes.department],
+    }),
+
+    // delete single department by id
+    deleteDepartment: build.mutation({
+      query: (id) => ({
+        url: `${DEPARTMENT_URL}/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: [tagTypes.department],
+    }),
   }),
 });
 
-export const { useAddDepartmentMutation } = departmentApi;
+export const {
+  useDepartmentsQuery,
+  useAddDepartmentMutation,
+  useDepartmentQuery, // get single department hooks
+  useUpdateDepartmentMutation, // update single department hooks
+  useDeleteDepartmentMutation, // delete single department hooks
+} = departmentApi;
